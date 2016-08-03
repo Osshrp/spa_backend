a3be633f90a59a08b4f2dc2c8f8c3c540a2aa737
Osshrp <osshrp@gmail.com>
2016-08-03 12:09:08 +0300

    finish post controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,26 +2,61 @@ module Api
   class PostsController < ApplicationController
 
     def index
-      @posts = Post.all
-      if @posts
+      if @posts = Post.all
         render json: @posts, status: 200
+      else
+        unsuccess_search 404
       end
     end
 
-    def new
-      @post = Post.new
+    def show
+      if find_post
+        render json: @post, status:200
+      else
+        unsuccess_search 404
+      end
     end
 
     def create
-    end
-
-    def edit
+      if @post = Post.create(post_params)
+        success_search 201
+      else
+        unsuccess_search 500
+      end
     end
 
     def update
+      if find_post.update(post_params)
+        success_search 200
+      else
+        unsuccess_search 404
+      end
     end
 
     def destroy
+      if find_post.destroy
+        success_search 200
+      else
+        unsuccess_search 404
+      end
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:username, :title, :body)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def success_search(http_status)
+      render json: { message: 'success' }, status: http_status
+    end
+
+    def unsuccess_search(http_status)
+      render nothing: true, status: http_status
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
-    get 'posts', to: 'posts#index'
-    # post 'cards', to: 'cards#create'
-    # put "review" => "trainer#review_card"
-    # get "train" => "trainer#index"
+    resources :posts
   end
 end
