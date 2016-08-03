9fa548b013b96a83ba5c264907644b10b9492e77
Osshrp <osshrp@gmail.com>
2016-08-03 06:27:38 +0300

    add post controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,4 +1,27 @@
 module Api
   class PostsController < ApplicationController
+
+    def index
+      @posts = Post.all
+      if @posts
+        render json: @posts, status: 200
+      end
+    end
+
+    def new
+      @post = Post.new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    get 'posts', to: 'posts#index'
+    # post 'cards', to: 'cards#create'
+    # put "review" => "trainer#review_card"
+    # get "train" => "trainer#index"
+  end
 end
