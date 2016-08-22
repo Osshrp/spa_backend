c2904874579546c6d5f4b622140ddb021a93ebe0
Osshrp <osshrp@gmail.com>
2016-08-22 15:43:03 +0300

    cors7

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://oss.bobrptd.ru.s3-website-us-west-2.amazonaws.com/'
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
